939610cbf6023207ee1cbccd5fa94ece8961485b
andrewplan <a.plan88@gmail.com>
2016-09-01 16:40:46 -0500

    Added local storage functionality to to-do list.  Consider adding a button that clears local storage.

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -3,10 +3,75 @@ $( document ).ready(function(){
 
   $('#newTaskForm').hide();
 
+  repopulateToDos( listo );
+
+  grabDataFromLocalStorage();
+
+  function grabDataFromLocalStorage() {
+    // if localStorage is not empty, parse it and store in listo.
+    if ( typeof localStorage[ 'todoData' ] !== 'undefined' ) {
+      listo = JSON.parse( localStorage[ 'todoData' ] );
+      repopulateToDos( listo );
+    }
+    // if localStorage is empty, set listo to equal []
+    else {
+      listo = [];
+    }
+  };
+
+  function repopulateToDos( arr ) {
+    // take what exists in listo now and add it back to the HTML
+    for ( var i = 0; i < arr.length; i++ ) {
+        var currentTaskName = arr[ i ][ 'task' ];
+        if ( arr[ i ].id === 'new' ) {
+          $('#newList').append(
+                  '<a href="#finish" class="" id="item">' +
+                  '<li class="list-group-item">' +
+                  '<h3>' + currentTaskName + '</h3>' +
+                  '<span class="arrow pull-right">' + '<i class="glyphicon glyphicon-arrow-right">' + '</span>' +
+                  '</li>' +
+                  '</a>'
+          );
+        }
+        else if ( arr[ i ].id === 'inProgress') {
+          $('#currentList').append(
+                  '<a href="#finish" class="" id="item">' +
+                  '<li class="list-group-item">' +
+                  '<h3>' + currentTaskName + '</h3>' +
+                  '<span class="arrow pull-right">' + '<i class="glyphicon glyphicon-arrow-right">' + '</span>' +
+                  '</li>' +
+                  '</a>'
+          );
+        }
+        else {
+          $('#archivedList').append(
+                  '<a href="#finish" class="" id="item">' +
+                  '<li class="list-group-item">' +
+                  '<h3>' + currentTaskName + '</h3>' +
+                  '<span class="arrow pull-right">' + '<i class="glyphicon glyphicon-arrow-right">' + '</span>' +
+                  '</li>' +
+                  '</a>'
+          );
+        }
+    }
+  };
+
+
+  var generateId = function() {
+    var currentId = 0;
+    return function() {
+      currentId += 1;
+      return currentId;
+    }
+  }
+
+  var idMaker = generateId();
+
   //task constructor that takes in the new task and wraps it in an object
   var Task = function( task ) {
     this.task = task;
     this.id = 'new';
+    this.index = idMaker();
   };
 
   var addTask = function( task ) {
@@ -15,6 +80,7 @@ $( document ).ready(function(){
       // pass new task into Task constructor and pushes it to 'listo' array
       task = new Task( task );
       listo.push( task );
+      localStorage[ 'todoData' ] = JSON.stringify( listo );
 
       $('#newItemInput').val('');
 
@@ -31,12 +97,14 @@ $( document ).ready(function(){
     $('#newTaskForm').slideToggle('fast', 'linear');
   };
 
+  addTask('chicken');
+
   var advanceTask = function( task ) {
     var modified = task.innerText.trim();
     for ( var i = 0; i < listo.length; i++ ) {
-      if ( listo[ i ].task === modified ) {
+      if ( listo[ i ][ 'task' ].toUpperCase() === modified ) {
         if ( listo[ i ].id === 'new' ) {
-          listo[ i ].id === 'inProgress';
+          listo[ i ].id = 'inProgress';
         }
         else if ( listo[ i ].id === 'inProgress') {
           listo[ i ].id = 'archived';
@@ -44,17 +112,22 @@ $( document ).ready(function(){
         else {
           listo.splice( i, 1 );
         }
+
         break;
       }
     }
+    localStorage[ 'todoData' ] = JSON.stringify( listo );
     task.remove();
   };
+
+
 
   //when click the 'saveNewItem' button, call the addTask function
   $('#saveNewItem').on('click', function( e ) {
     e.preventDefault();
     var task = $('#newItemInput').val().trim();
     addTask( task );
+
   });
 
   //opens form
@@ -70,6 +143,7 @@ $( document ).ready(function(){
 
   //moves task to 'In Progress'
   $( document ).on( 'click', '#item', function( e ) {
+    // debugger;
     e.preventDefault();
     var task = this;
     advanceTask(task);
